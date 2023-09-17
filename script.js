9d04e1fce63d73cf2614d07ba8273eb690c76c94
Aman Kumar Gupta <122474163+Amangupta149@users.noreply.github.com>
2023-09-17 08:53:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+
+  if (inputBox.value === '') {
+    alert("Please write Something!");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveTask();
+}
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("done");
+  }
+  else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveTask();
+  }
+}, false);
+
+function saveTask() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
